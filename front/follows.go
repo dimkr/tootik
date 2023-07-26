3f616951214c86fe204c2febacc3a845d060d211
Dima Krasner <dima@dimakrasner.com>
2023-07-26 20:23:14 +0300

    count group posts in followed users list

--- front/follows.go
+++ front/follows.go
@@ -43,7 +43,7 @@ func follows(w text.Writer, r *request) {
 		return
 	}
 
-	rows, err := r.Query(`with followed as (select followed as id, inserted from follows where follower = ?) select persons.actor, stats.last, stats.count from followed join (select id, actor from persons) persons on persons.id = followed.id left join (select author, max(inserted) as last, count(*) as count from notes where inserted >= unixepoch() - 7*24*60*60 and exists (select 1 from followed where notes.author = followed.id) group by author) stats on stats.author = followed.id order by stats.last desc, followed.inserted desc`, r.User.ID)
+	rows, err := r.Query(`with followed as (select followed as id, inserted from follows where follower = ?) select persons.actor, max(notes.inserted) as last, count(distinct notes.id) as count from followed join (select id, actor from persons) persons on persons.id = followed.id left join (select * from notes where inserted >= unixepoch() - 7*24*60*60) notes on (persons.actor->>'type' != 'Group' and notes.author = followed.id) or (persons.actor->>'type' = 'Group' and notes.object->'inReplyTo' is null and (followed.id in (notes.cc0, notes.to0, notes.cc1, notes.to1, notes.cc2, notes.to2) or (notes.cc2 is not null and exists (select 1 from json_each(notes.object->'cc') where value = followed.id)) or (notes.to2 is not null and exists (select 1 from json_each(notes.object->'to') where value = followed.id)))) group by followed.id order by last desc, followed.inserted desc`, r.User.ID)
 	if err != nil {
 		r.Log.WithField("follower", r.User.ID).WithError(err).Warn("Failed to list followed users")
 		w.Error()
