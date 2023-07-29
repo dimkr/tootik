54e71116b0a97a3abf820c2fa9612d04dc1d0a30
Dima Krasner <dima@dimakrasner.com>
2023-07-29 15:21:50 +0000

    add migrations infrastructure

---
+++ migrations/migration.go
@@ -0,0 +1,60 @@
+/*
+Copyright 2023 Dima Krasner
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package migrations
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+)
+
+type migration struct {
+	ID string
+	Up func(context.Context, *sql.DB) error
+}
+
+//go:generate ./list.sh
+
+func Run(ctx context.Context, db *sql.DB, logger *log.Logger) error {
+	if _, err := db.ExecContext(ctx, `create table if not exists migrations(id string not null primary key, applied integer default (unixepoch()))`); err != nil {
+		return err
+	}
+
+	for _, m := range migrations {
+		var applied string
+		if err := db.QueryRowContext(ctx, `select datetime(applied, 'unixepoch') from migrations where id = ?`, m.ID).Scan(&applied); err != nil && !errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("Failed to check if %s is applied: %w", m.ID, err)
+		} else if err == nil {
+			logger.WithFields(log.Fields{"id": m.ID, "applied": applied}).Debug("Skipping migration")
+			continue
+		}
+
+		logger.WithField("id", m.ID).Info("Applying migration")
+
+		if err := m.Up(ctx, db); err != nil {
+			return fmt.Errorf("Failed to apply %s: %w", m.ID, err)
+		}
+
+		if _, err := db.ExecContext(ctx, `insert into migrations(id) values (?)`, m.ID); err != nil {
+			return fmt.Errorf("Failed to record %s: %w", m.ID, err)
+		}
+	}
+
+	return nil
+}
