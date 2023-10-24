03cfb0d553b5696ecb6c9e8a133d9e91ff9162d6
Dima Krasner <dima@dimakrasner.com>
2023-10-24 18:11:26 +0300

    add support for out-of-order packets

--- front/guppy/guppy.go
+++ front/guppy/guppy.go
@@ -36,18 +36,26 @@ import (
 	"time"
 )
 
+type incomingPacket struct {
+	Data []byte
+	From net.Addr
+}
+
+type responseChunk struct {
+	Data  []byte
+	Seq   int
+	Acked bool
+	Sent  time.Time
+}
+
 const (
 	maxRequestSize    = 1024
 	responseChunkSize = 512
+	maxUnackedChunks  = 8
 	reqTimeout        = time.Second * 30
 	maxSessions       = 32
-	retryInterval     = time.Second * 2
+	resendInterval    = time.Second * 2
 )
-
-type packet struct {
-	Payload []byte
-	From    net.Addr
-}
 
 func handle(ctx context.Context, log *slog.Logger, db *sql.DB, handler front.Handler, resolver *fed.Resolver, wg *sync.WaitGroup, from net.Addr, req []byte, acks <-chan []byte, done chan<- string, s net.PacketConn) {
 	defer func() {
@@ -90,22 +98,38 @@ func handle(ctx context.Context, log *slog.Logger, db *sql.DB, handler front.Han
 		return
 	}
 
-	prevPacket := chunk[:n]
+	chunks := make([]responseChunk, 1, buf.Len()/responseChunkSize+2)
+	chunks[0].Seq = seq
+	chunks[0].Data = chunk[:n]
 
 	// fix the sequence number if the response is cached
 	// TODO: something less ugly
 	space := bytes.IndexByte(chunk[:n], ' ')
 	if string(chunk[:space]) != "0" && string(chunk[:space]) != "1" {
-		prevPacket = append([]byte(fmt.Sprintf("%d", seq)), chunk[space:n]...)
+		chunks[0].Data = append([]byte(fmt.Sprintf("%d", seq)), chunk[space:n]...)
 	}
 
-	s.WriteTo(prevPacket, from)
+	for {
+		seq++
+		statusLine := fmt.Sprintf("%d\r\n", seq)
+		n, err := buf.Read(chunk)
+		if err != nil && errors.Is(err, io.EOF) {
+			// this is the EOF packet
+			chunks = append(chunks, responseChunk{Data: []byte(statusLine), Seq: seq})
+			break
+		} else if err != nil {
+			log.Error("Failed to read respone chunk", "error", err)
+			return
+		}
+		chunks = append(chunks, responseChunk{Data: append([]byte(statusLine), chunk[:n]...), Seq: seq})
+	}
 
-	retry := time.NewTicker(retryInterval)
+	retry := time.NewTicker(resendInterval)
 	defer retry.Stop()
 
-	expectedAck := fmt.Sprintf("%d\r\n", seq)
-	finSent := false
+	log.Debug("Sending response", "path", reqUrl.Path, "from", from, "first", chunks[0].Seq, "last", chunks[len(chunks)-1].Seq, "chunks", len(chunks))
+
+	send := true
 
 	for {
 		select {
@@ -119,38 +143,67 @@ func handle(ctx context.Context, log *slog.Logger, db *sql.DB, handler front.Han
 				return
 			}
 
-			if string(ack) != expectedAck {
-				log.Debug("Received invalid ack", "path", reqUrl.Path, "from", from, "expected", expectedAck, "got", string(ack))
+			var ackedSeq int
+			n, err := fmt.Sscanf(string(ack), "%d\r\n", &ackedSeq)
+			if err != nil {
+				log.Debug("Received invalid ack", "path", reqUrl.Path, "from", from, "ack", string(ack), "error", err)
+				continue
+			}
+			if n < 1 {
+				log.Debug("Received invalid ack", "path", reqUrl.Path, "from", from, "ack", string(ack))
 				continue
 			}
 
-			if finSent {
+			i := ackedSeq - chunks[0].Seq
+			if i < 0 || i >= len(chunks) {
+				log.Debug("Received invalid ack", "path", reqUrl.Path, "from", from, "ack", string(ack))
+				continue
+			}
+
+			if chunks[i].Acked {
+				log.Debug("Received duplicate ack", "path", reqUrl.Path, "from", from, "acked", ackedSeq)
+				continue
+			}
+
+			log.Debug("Marking packet as received", "path", reqUrl.Path, "from", from, "acked", ackedSeq)
+			chunks[i].Acked = true
+
+			// stop if the acked packet is the EOF packet
+			if i == len(chunks)-1 {
 				return
 			}
 
-			seq++
-			statusLine := fmt.Sprintf("%d\r\n", seq)
-			n, err := buf.Read(chunk)
-			if err != nil && errors.Is(err, io.EOF) {
-				prevPacket = []byte(statusLine)
-			} else if err != nil {
-				log.Error("Failed to read respone chunk", "error", err)
-				return
-			} else {
-				prevPacket = append([]byte(statusLine), chunk[:n]...)
-			}
-
-			s.WriteTo(prevPacket, from)
-
-			finSent = n == 0
-			expectedAck = statusLine
-
-			retry.Reset(retryInterval)
+			send = true
+			retry.Reset(resendInterval)
 
 		case <-retry.C:
-			log.Debug("Resending previous packet", "path", reqUrl.Path, "from", from)
-			s.WriteTo(prevPacket, from)
+			send = true
 		}
+
+		if !send {
+			continue
+		}
+
+		now := time.Now()
+		sent := 0
+		for _, chunk := range chunks {
+			if chunk.Acked || now.Sub(chunk.Sent) <= resendInterval {
+				continue
+			}
+			if sent == maxUnackedChunks {
+				break
+			}
+			if chunk.Sent == (time.Time{}) {
+				log.Debug("Sending packet", "path", reqUrl.Path, "from", from, "seq", chunk.Seq)
+			} else {
+				log.Debug("Resending packet", "path", reqUrl.Path, "from", from, "seq", chunk.Seq)
+			}
+			s.WriteTo(chunk.Data, from)
+			chunk.Sent = now
+			sent++
+		}
+
+		send = false
 	}
 }
 
@@ -162,18 +215,21 @@ func ListenAndServe(ctx context.Context, log *slog.Logger, db *sql.DB, handler f
 
 	var wg sync.WaitGroup
 
-	packets := make(chan packet)
+	incoming := make(chan incomingPacket)
 
 	wg.Add(1)
 	go func() {
+		defer close(incoming)
+		defer wg.Done()
+
 		buf := make([]byte, maxRequestSize)
 		for {
 			n, from, err := l.ReadFrom(buf)
 			if err != nil {
-				log.Warn("Failed to receive a packet", "error", err)
-				continue
+				log.Error("Failed to receive a packet", "error", err)
+				return
 			}
-			packets <- packet{buf[:n], from}
+			incoming <- incomingPacket{buf[:n], from}
 		}
 	}()
 
@@ -182,20 +238,34 @@ func ListenAndServe(ctx context.Context, log *slog.Logger, db *sql.DB, handler f
 
 loop:
 	for {
+		keepClosing := true
+
+		for keepClosing {
+			select {
+			case k := <-done:
+				acks := sessions[k]
+				close(acks)
+				delete(sessions, k)
+
+			default:
+				keepClosing = false
+			}
+		}
+
 		select {
 		case <-ctx.Done():
 			break loop
 
-		case k := <-done:
-			acks := sessions[k]
-			close(acks)
-			delete(sessions, k)
-
-		case pkt := <-packets:
+		case pkt, ok := <-incoming:
+			if !ok {
+				break loop
+			}
 			k := pkt.From.String()
 
 			if acks, ok := sessions[k]; ok {
-				acks <- pkt.Payload
+				if len(acks) < maxUnackedChunks {
+					acks <- pkt.Data
+				}
 				continue
 			}
 			if len(sessions) > maxSessions {
@@ -204,14 +274,14 @@ loop:
 				continue
 			}
 
-			acks := make(chan []byte, 1)
+			acks := make(chan []byte, maxUnackedChunks)
 			sessions[k] = acks
 
 			requestCtx, cancelRequest := context.WithTimeout(ctx, reqTimeout)
 
 			wg.Add(1)
 			go func() {
-				handle(requestCtx, log, db, handler, resolver, &wg, pkt.From, pkt.Payload, acks, done, l)
+				handle(requestCtx, log, db, handler, resolver, &wg, pkt.From, pkt.Data, acks, done, l)
 				cancelRequest()
 				wg.Done()
 			}()
