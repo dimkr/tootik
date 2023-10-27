73e44888afae7beecceaf3130b728da976971741
Dima Krasner <dima@dimakrasner.com>
2023-10-27 13:05:20 +0300

    fix empty i line after graphs

---
+++ front/text/gmap/writer_test.go
@@ -0,0 +1,57 @@
+/*
+Copyright 2023 Dima Krasner
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gmap
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRaw_TrailingNewLine(t *testing.T) {
+	assert := assert.New(t)
+
+	var b bytes.Buffer
+	w := Wrap(&b)
+
+	w.Raw(
+		"Alt text",
+		"  _\n\\'_'/\n|___|\n/   \\\n",
+	)
+
+	assert.Equal(
+		"i  _\t/\t0\t0\r\ni\\'_'/\t/\t0\t0\r\ni|___|\t/\t0\t0\r\ni/   \\\t/\t0\t0\r\n",
+		b.String(),
+	)
+}
+
+func TestRaw_NoTrailingNewLine(t *testing.T) {
+	assert := assert.New(t)
+
+	var b bytes.Buffer
+	w := Wrap(&b)
+
+	w.Raw(
+		"Alt text",
+		"  _\n\\'_'/\n|___|\n/   \\",
+	)
+
+	assert.Equal(
+		"i  _\t/\t0\t0\r\ni\\'_'/\t/\t0\t0\r\ni|___|\t/\t0\t0\r\ni/   \\\t/\t0\t0\r\n",
+		b.String(),
+	)
+}
