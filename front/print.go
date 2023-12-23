c387ae9f381c9424f793f3274158e6d45474e991
Dima Krasner <dima@dimakrasner.com>
2023-12-23 16:14:26 +0200

    collapse multiple empty lines in compact view

--- front/print.go
+++ front/print.go
@@ -62,12 +62,28 @@ func getTextAndLinks(s string, maxRunes, maxLines int) ([]string, data.OrderedMa
 	lines := strings.Split(raw, "\n")
 
 	if maxLines > 0 && len(lines) > maxLines {
-		for i := maxLines - 1; i >= 0; i-- {
-			if i == 0 || strings.TrimSpace(lines[i]) != "" {
-				lines[i+1] = "[…]"
-				return lines[:i+2], links
+		summary := make([]string, 0, maxLines)
+		lastNonEmptyLine := 0
+		for _, line := range lines {
+			if len(summary) == maxLines {
+				break
+			}
+			line = strings.TrimSpace(line)
+			if line != "" {
+				summary = append(summary, line)
+				lastNonEmptyLine = len(summary) - 1
+				// replace multiple empty lines with one […] line
+			} else if len(summary) > 0 && (len(summary) > 0 && summary[len(summary)-1] == "") {
+				summary[len(summary)-1] = "[…]"
+			} else if len(summary) == 0 || summary[len(summary)-1] != "[…]" {
+				summary = append(summary, line)
 			}
 		}
+		// terminate with a single […] line if the post is truncated
+		if len(summary) == maxLines || lastNonEmptyLine == maxLines-1 || summary[len(summary)-1] == "" {
+			summary[len(summary)-1] = "[…]"
+		}
+		return summary, links
 	}
 
 	return lines, links
