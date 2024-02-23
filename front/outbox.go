b68120ca48024494839687fb975dc659cd44e3b6
Dima Krasner <dima@dimakrasner.com>
2024-02-23 12:09:12 +0200

    hide sharer tag in group outbox

--- front/outbox.go
+++ front/outbox.go
@@ -55,9 +55,8 @@ func (h *Handler) userOutbox(w text.Writer, r *request, args ...string) {
 	if actor.Type == ap.Group && r.User == nil {
 		// unauthenticated users can only see public posts in a group
 		rows, err = r.Query(
-			`select notes.object, authors.actor, sharers.actor from notes
+			`select notes.object, authors.actor, null from notes
 			join persons authors on authors.id = notes.author
-			join persons sharers on sharers.id = $1
 			left join notes replies on replies.object->>'inReplyTo' = notes.id
 			where notes.object->>'audience' = $1 and notes.public = 1 and notes.object->>'inReplyTo' is null and (replies.inserted is null or replies.inserted > unixepoch() - 86400)
 			group by notes.id
@@ -69,9 +68,8 @@ func (h *Handler) userOutbox(w text.Writer, r *request, args ...string) {
 	} else if actor.Type == ap.Group && r.User != nil {
 		// users can see public posts in a group and non-public posts if they follow the group
 		rows, err = r.Query(
-			`select notes.object, authors.actor, sharers.actor from notes
+			`select notes.object, authors.actor, null from notes
 			join persons authors on authors.id = notes.author
-			join persons sharers on sharers.id = $1
 			left join notes replies on replies.object->>'inReplyTo' = notes.id
 			where
 				notes.object->>'audience' = $1 and
