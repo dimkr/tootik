447f45966eab8dabca73b17b8e38bb6d1d1baaa0
Dima Krasner <dima@dimakrasner.com>
2024-02-26 09:28:04 +0200

    cleanup

--- migrations/027_sqlite345.go
+++ migrations/027_sqlite345.go
@@ -30,26 +30,26 @@ func sqlite345(ctx context.Context, domain string, tx *sql.Tx) error {
 		return err
 	}
 
-	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN to0 STRING AS (object->'to'->>'[0]')`); err != nil {
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN to0 STRING AS (object->>'$.to[0]')`); err != nil {
 		return err
 	}
 
-	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN to1 STRING AS (object->'to'->>'[1]')`); err != nil {
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN to1 STRING AS (object->>'$.to[1]')`); err != nil {
 		return err
 	}
 
-	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN to2 STRING AS (object->'to'->>'[2]')`); err != nil {
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN to2 STRING AS (object->>'$.to[2]')`); err != nil {
 		return err
 	}
 
-	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN cc0 STRING AS (object->'cc'->>'[0]')`); err != nil {
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN cc0 STRING AS (object->>'$.cc[0]')`); err != nil {
 		return err
 	}
 
-	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN cc1 STRING AS (object->'cc'->>'[1]')`); err != nil {
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN cc1 STRING AS (object->>'$.cc[1]')`); err != nil {
 		return err
 	}
 
-	_, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN cc2 STRING AS (object->'cc'->>'[2]')`)
+	_, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN cc2 STRING AS (object->>'$.cc[2]')`)
 	return err
 }
