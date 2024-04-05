f53d2c25db8d39627c309b31f061cef842ac69a6
Dima Krasner <dima@dimakrasner.com>
2024-04-05 10:40:06 +0300

    cleanup

--- fed/icon/icon.go
+++ fed/icon/icon.go
@@ -17,55 +17,7 @@ limitations under the License.
 // Package icon generates tiny, pseudo-random user avatars.
 package icon
 
-import (
-	"bytes"
-	"crypto/sha256"
-	"image"
-	"image/color"
-	"image/draw"
-	"image/gif"
-)
-
 const (
 	MediaType         = "image/gif"
 	FileNameExtension = ".gif"
 )
-
-// Generate generates a tiny pseudo-random image by user name
-func Generate(s string) ([]byte, error) {
-	hash := sha256.Sum256([]byte(s))
-
-	fg := color.RGBA{128 + (hash[0]^hash[29])%128, 128 + (hash[1]^hash[30])%128, 128 + (hash[2]^hash[31])%128, 255}
-	alt := []color.RGBA{
-		color.RGBA{fg.R, fg.B, fg.G, 255},
-		color.RGBA{fg.G, fg.B, fg.R, 255},
-		color.RGBA{fg.G, fg.R, fg.B, 255},
-		color.RGBA{fg.B, fg.R, fg.G, 255},
-		color.RGBA{fg.B, fg.G, fg.R, 255},
-	}[hash[0]%5]
-	bg := color.RGBA{255 - fg.R, 255 - fg.G, 255 - fg.B, 255}
-
-	m := image.NewPaletted(image.Rect(0, 0, 8, 8), color.Palette{bg, fg, alt})
-	draw.Draw(m, m.Bounds(), &image.Uniform{bg}, image.Point{}, draw.Src)
-
-	for i, b := range hash[:16] {
-		c := fg
-		if hash[16+i]%8 == 0 {
-			c = alt
-		}
-
-		if (b^hash[16+i])%2 == 1 {
-			m.Set(i%4, i/4, c)
-			m.Set(i%4, 7-i/4, c)
-			m.Set(7-i%4, 7-i/4, c)
-			m.Set(7-i%4, i/4, c)
-		}
-	}
-
-	var buf bytes.Buffer
-	if err := gif.Encode(&buf, m, &gif.Options{NumColors: 3}); err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
-}
