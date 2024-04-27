7d4c2592944a16fac04dc78e6b3570087e573409
Dima Krasner <dima@dimakrasner.com>
2024-04-27 17:33:43 +0300

    oops

--- outbox/forward.go
+++ outbox/forward.go
@@ -168,17 +168,21 @@ func ForwardActivity(ctx context.Context, domain string, cfg *cfg.Config, log *s
 		return nil
 	}
 
-	var firstPostID, threadStarterID string
-	var depth int
-	if err := tx.QueryRowContext(ctx, `with recursive thread(id, author, parent, depth) as (select notes.id, notes.author, notes.object->>'$.inReplyTo' as parent, 1 as depth from notes where id = $1 union all select notes.id, notes.author, notes.object->>'$.inReplyTo' as parent, t.depth + 1 from thread t join notes on notes.id = t.parent where t.depth <= $2) select id, author, depth from thread order by depth desc limit 1`, note.ID, cfg.MaxForwardingDepth+1).Scan(&firstPostID, &threadStarterID, &depth); err != nil && errors.Is(err, sql.ErrNoRows) {
-		log.Debug("Failed to find thread for post", "note", note.ID)
-		return nil
-	} else if err != nil {
-		return fmt.Errorf("failed to fetch first post in thread: %w", err)
-	}
-	if depth > cfg.MaxForwardingDepth {
-		log.Debug("Thread exceeds depth limit for forwarding")
-		return nil
+	firstPostID := note.ID
+	var threadStarterID string
+
+	if note.InReplyTo != "" {
+		var depth int
+		if err := tx.QueryRowContext(ctx, `with recursive thread(id, author, parent, depth) as (select notes.id, notes.author, notes.object->>'$.inReplyTo' as parent, 1 as depth from notes where id = $1 union all select notes.id, notes.author, notes.object->>'$.inReplyTo' as parent, t.depth + 1 from thread t join notes on notes.id = t.parent where t.depth <= $2) select id, author, depth from thread order by depth desc limit 1`, note.ID, cfg.MaxForwardingDepth+1).Scan(&firstPostID, &threadStarterID, &depth); err != nil && errors.Is(err, sql.ErrNoRows) {
+			log.Debug("Failed to find thread for post", "note", note.ID)
+			return nil
+		} else if err != nil {
+			return fmt.Errorf("failed to fetch first post in thread: %w", err)
+		}
+		if depth > cfg.MaxForwardingDepth {
+			log.Debug("Thread exceeds depth limit for forwarding")
+			return nil
+		}
 	}
 
 	forwarded, err := forwardToGroup(ctx, domain, log, tx, note, rawActivity, firstPostID)
