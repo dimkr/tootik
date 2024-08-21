a9a8727f9511a69786a5816c4965125d90f8e89a
Dima Krasner <dima@dimakrasner.com>
2024-08-21 21:31:07 +0300

    cleanup

--- test/unfollow_test.go
+++ test/unfollow_test.go
@@ -48,6 +48,30 @@ func TestUnfollow_HappyFlow(t *testing.T) {
 	assert.NoError((inbox.FeedUpdater{Domain: domain, Config: server.cfg, DB: server.db}).Run(context.Background()))
 
 	users = server.Handle("/users", server.Alice)
+	assert.Contains(users, "Hello followers")
+}
+
+func TestUnfollow_HappyFlowBeforeFeedUpdate(t *testing.T) {
+	server := newTestServer()
+	defer server.Shutdown()
+
+	assert := assert.New(t)
+
+	follow := server.Handle("/users/follow/"+strings.TrimPrefix(server.Bob.ID, "https://"), server.Alice)
+	assert.Equal(fmt.Sprintf("30 /users/outbox/%s\r\n", strings.TrimPrefix(server.Bob.ID, "https://")), follow)
+
+	say := server.Handle("/users/whisper?Hello%20followers", server.Bob)
+	assert.Regexp(`^30 /users/view/\S+\r\n$`, say)
+
+	users := server.Handle("/users", server.Alice)
+	assert.NotContains(users, "Hello followers")
+
+	unfollow := server.Handle("/users/unfollow/"+strings.TrimPrefix(server.Bob.ID, "https://"), server.Alice)
+	assert.Equal(fmt.Sprintf("30 /users/outbox/%s\r\n", strings.TrimPrefix(server.Bob.ID, "https://")), unfollow)
+
+	assert.NoError((inbox.FeedUpdater{Domain: domain, Config: server.cfg, DB: server.db}).Run(context.Background()))
+
+	users = server.Handle("/users", server.Alice)
 	assert.NotContains(users, "Hello followers")
 }
 
@@ -74,7 +98,7 @@ func TestUnfollow_FollowAgain(t *testing.T) {
 	assert.NoError((inbox.FeedUpdater{Domain: domain, Config: server.cfg, DB: server.db}).Run(context.Background()))
 
 	users = server.Handle("/users", server.Alice)
-	assert.NotContains(users, "Hello followers")
+	assert.Contains(users, "Hello followers")
 
 	follow = server.Handle("/users/follow/"+strings.TrimPrefix(server.Bob.ID, "https://"), server.Alice)
 	assert.Equal(fmt.Sprintf("30 /users/outbox/%s\r\n", strings.TrimPrefix(server.Bob.ID, "https://")), follow)
