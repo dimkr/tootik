b0abcc70e80e4d2e2cceb8b8b256a74564d38082
Dima Krasner <dima@dimakrasner.com>
2025-01-12 09:41:08 +0200

    oops

--- inbox/feed.go
+++ inbox/feed.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 Dima Krasner
+Copyright 2024, 2025 Dima Krasner
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-
 	"github.com/dimkr/tootik/cfg"
 )
 
