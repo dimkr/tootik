2500b7281057906d52fb8690d8fb73045be7ed2d
Dima Krasner <dima@dimakrasner.com>
2025-04-21 20:54:25 +0300

    oops

---
+++ migrations/039_reject.go
@@ -0,0 +1,35 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+)
+
+func reject(ctx context.Context, domain string, tx *sql.Tx) error {
+	if _, err := tx.ExecContext(ctx, `DROP INDEX followsfollower`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `DROP INDEX followsfollowed`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `CREATE TABLE newfollows(id STRING NOT NULL PRIMARY KEY, follower STRING NOT NULL, inserted INTEGER DEFAULT (UNIXEPOCH()), accepted INTEGER, followed STRING)`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `DROP TABLE follows`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE newfollows RENAME TO follows`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `CREATE INDEX followsfollower ON follows(follower)`); err != nil {
+		return err
+	}
+
+	_, err := tx.ExecContext(ctx, `CREATE INDEX followsfollowed ON follows(followed)`)
+	return err
+}
