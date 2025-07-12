6c5ce64227220c986064f4d4c023e1494df75721
Dima Krasner <dima@dimakrasner.com>
2025-07-12 15:46:28 +0300

    wip

---
+++ ap/capability.go
@@ -0,0 +1,28 @@
+/*
+Copyright 2025 Dima Krasner
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ap
+
+// Capability is a capability that may be supported by an ActivityPub server.
+type Capability uint
+
+const (
+	// RFC9421Signatures is support for RFC9421 HTTP signatures.
+	RFC9421Signatures Capability = 1
+
+	// RFC9421ED25519Signatures is support for RFC9421 HTTP signatures, with Ed25119 keys.
+	RFC9421ED25519Signatures Capability = 2
+)
