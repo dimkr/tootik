c729bc0cd75d65a5d7590fea8c885b8a8212ee94
Dima Krasner <dima@dimakrasner.com>
2025-09-22 15:22:40 +0300

    wip

--- logcontext/logcontext.go
+++ logcontext/logcontext.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package logcontext enriches logs with additional context.
 package logcontext
 
 import (
