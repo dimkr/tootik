ce1d7d928c01d9113e06cf15811b3e14c651fad7
Dima Krasner <dima@dimakrasner.com>
2025-09-26 09:44:10 +0300

    deliver activities once per host, not per inbox

---
+++ migrations/048_deliverieshost.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+)
+
+func deliverieshost(ctx context.Context, domain string, tx *sql.Tx) error {
+	if _, err := tx.ExecContext(ctx, `CREATE TABLE ndeliveries(activity TEXT NOT NULL, host TEXT NOT NULL)`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `INSERT INTO ndeliveries(activity, host) SELECT distinct activity, substr(substr(inbox, 9), 0, instr(substr(inbox, 9), '/')) FROM deliveries`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `DROP TABLE deliveries`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE ndeliveries RENAME TO deliveries`); err != nil {
+		return err
+	}
+
+	if _, err := tx.ExecContext(ctx, `CREATE INDEX deliveriesactivity ON deliveries(activity)`); err != nil {
+		return err
+	}
+
+	return nil
+}
