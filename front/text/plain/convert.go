dimkr/tootik

commit 28ded3e31442b4d07a15ee8e3d8875b14aa66ac1

if unused, convert []byte to string without allocating and copying

front/text/plain/convert.go
@@ -27,6 +27,7 @@ import (
 	"strings"
 
 	"github.com/dimkr/tootik/ap"
+	"github.com/dimkr/tootik/danger"
 	"github.com/dimkr/tootik/data"
 	tokenizer "golang.org/x/net/html"
 )
@@ -90,7 +91,7 @@ func fromHTML(text string) (string, data.OrderedMap[string, string], error) {
 
 		case tokenizer.EndTagToken:
 			tagBytes, _ := tok.TagName()
-			tag := string(tagBytes)
+			tag := danger.String(tagBytes)
 
 			if len(openTags) > 0 && tag == openTags[len(openTags)-1] {
 				openTags = openTags[:len(openTags)-1]
@@ -142,7 +143,7 @@ func fromHTML(text string) (string, data.OrderedMap[string, string], error) {
 
 		case tokenizer.StartTagToken, tokenizer.SelfClosingTagToken:
 			tagBytes, hasAttrs := tok.TagName()
-			tag := string(tagBytes)
+			tag := danger.String(tagBytes)
 
 			if tag == "br" {
 				w.WriteByte('\n')
@@ -205,8 +206,8 @@ func fromHTML(text string) (string, data.OrderedMap[string, string], error) {
 				for {
 					attrBytes, value, more := tok.TagAttr()
 
-					attr := string(attrBytes)
-					valueString := string(value)
+					attr := danger.String(attrBytes)
+					valueString := danger.String(value)
 					if tt == tokenizer.StartTagToken && tag == "span" && attr == "class" {
 						if valueString == "invisible" {
 							invisibleDepth = len(openTags)

ap/activity.go
@@ -22,6 +22,8 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+
+	"github.com/dimkr/tootik/danger"
 )
 
 type ActivityType string
@@ -155,8 +157,7 @@ func (a *Activity) Scan(src any) error {
 }
 
 func (a *Activity) Value() (driver.Value, error) {
-	buf, err := json.Marshal(a)
-	return string(buf), err
+	return danger.MarshalJSON(a)
 }
 
 func (a *Activity) LogValue() slog.Value {

ap/actor.go
@@ -20,6 +20,8 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+
+	"github.com/dimkr/tootik/danger"
 )
 
 type ActorType string
@@ -71,6 +73,5 @@ func (a *Actor) Scan(src any) error {
 }
 
 func (a *Actor) Value() (driver.Value, error) {
-	buf, err := json.Marshal(a)
-	return string(buf), err
+	return danger.MarshalJSON(a)
 }

ap/attachment.go
@@ -20,6 +20,8 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+
+	"github.com/dimkr/tootik/danger"
 )
 
 type AttachmentType string
@@ -50,6 +52,5 @@ func (a *Attachment) Scan(src any) error {
 }
 
 func (a *Attachment) Value() (driver.Value, error) {
-	buf, err := json.Marshal(a)
-	return string(buf), err
+	return danger.MarshalJSON(a)
 }

ap/audience.go
@@ -21,6 +21,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/dimkr/tootik/danger"
 	"github.com/dimkr/tootik/data"
 )
 
@@ -89,6 +90,5 @@ func (a *Audience) Scan(src any) error {
 }
 
 func (a *Audience) Value() (driver.Value, error) {
-	buf, err := json.Marshal(a)
-	return string(buf), err
+	return danger.MarshalJSON(a)
 }

ap/object.go
@@ -20,6 +20,8 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+
+	"github.com/dimkr/tootik/danger"
 )
 
 type ObjectType string
@@ -85,6 +87,5 @@ func (o *Object) Scan(src any) error {
 }
 
 func (o *Object) Value() (driver.Value, error) {
-	buf, err := json.Marshal(o)
-	return string(buf), err
+	return danger.MarshalJSON(o)
 }

cmd/tootik/main.go
@@ -37,6 +37,7 @@ import (
 	"github.com/dimkr/tootik/ap"
 	"github.com/dimkr/tootik/buildinfo"
 	"github.com/dimkr/tootik/cfg"
+	"github.com/dimkr/tootik/danger"
 	"github.com/dimkr/tootik/data"
 	"github.com/dimkr/tootik/fed"
 	"github.com/dimkr/tootik/front"
@@ -288,7 +289,7 @@ func main() {
 			ctx,
 			"insert into icons(name, buf) values($1, $2) on conflict(name) do update set buf = $2",
 			userName,
-			string(resized),
+			danger.String(resized),
 		); err != nil {
 			panic(err)
 		}

danger/danger.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 - 2025 Dima Krasner
+Copyright 2025 Dima Krasner
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,24 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package ap
-
-import (
-	"time"
-
-	"github.com/dimkr/tootik/danger"
-)
-
-// Time is a wrapper around [time.Time] with fallback if parsing of RFC3339 fails.
-type Time struct {
-	time.Time
-}
-
-func (t *Time) UnmarshalJSON(b []byte) error {
-	err := t.Time.UnmarshalJSON(b)
-	// ugly hack for Threads
-	if err != nil && len(b) > 2 && b[0] == '"' && b[len(b)-1] == '"' {
-		t.Time, err = time.Parse("2006-01-02T15:04:05-0700", danger.String(b[1:len(b)-1]))
-	}
-	return err
-}
+// Package danger provides memory unsafe utilities.
+package danger

commit f571c0712bc97eeec870541003d3976917658595

fix breakge of HTML to gemtext conversion introduced by 28ded3e

front/text/plain/convert.go
@@ -155,7 +155,14 @@ func fromHTML(text string) (string, data.OrderedMap[string, string], error) {
 					return "", nil, errors.New("too nested")
 				}
 
-				openTags = append(openTags, tag)
+				/*
+					[golang.org/x/net/html.Tokenizer.TagName] docs say:
+
+						The contents of the returned slice may change on the next call to Next.
+
+					therefore we must copy the string when appending to openTags
+				*/
+				openTags = append(openTags, string(tagBytes))
 			}
 
 			if tag == "ul" {
@@ -207,21 +214,21 @@ func fromHTML(text string) (string, data.OrderedMap[string, string], error) {
 					attrBytes, value, more := tok.TagAttr()
 
 					attr := danger.String(attrBytes)
-					valueString := danger.String(value)
 					if tt == tokenizer.StartTagToken && tag == "span" && attr == "class" {
+						valueString := danger.String(value)
 						if valueString == "invisible" {
 							invisibleDepth = len(openTags)
 						} else if valueString == "ellipsis" {
 							ellipsisDepth = len(openTags)
 						}
 					} else if tag == "a" && attr == "class" {
-						class = valueString
+						class = string(value)
 					} else if tag == "a" && attr == "href" {
-						href = valueString
+						href = string(value)
 					} else if tag == "img" && attr == "alt" {
-						alt = valueString
+						alt = string(value)
 					} else if tag == "img" && attr == "src" {
-						src = valueString
+						src = string(value)
 					}
 
 					if !more {