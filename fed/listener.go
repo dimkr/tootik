f67ba4b6ee07c635ef96da007202306cdbefb8a9
Dima Krasner <dima@dimakrasner.com>
2025-12-03 20:11:07 +0200

    x

--- fed/listener.go
+++ fed/listener.go
@@ -26,7 +26,6 @@ import (
 	"net"
 	"net/http"
 	"path/filepath"
-	"strings"
 	"sync"
 	"time"
 
@@ -62,7 +61,7 @@ func (l *Listener) newHandler() (*http.ServeMux, error) {
 	mux.HandleFunc("GET /actor", l.handleActor)
 	mux.HandleFunc("GET /icon/{username}", l.handleIcon)
 	mux.HandleFunc("POST /inbox/{username}", l.handleInbox)
-	mux.HandleFunc("POST "+strings.TrimPrefix(l.AppActor.Inbox, "https://"+l.Domain), l.handleSharedInbox)
+	mux.HandleFunc("POST /inbox/actor", l.handleSharedInbox)
 	mux.HandleFunc("POST /inbox", l.handleSharedInbox) // PieFed falls back https://$domain/inbox if it can't fetch instance actor
 	mux.HandleFunc("GET /outbox/{username}", l.handleOutbox)
 	mux.HandleFunc("GET /post/{hash}", l.handlePost)
