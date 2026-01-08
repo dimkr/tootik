a7d40450c90d38d2445f7ff0860f88d832275880
Dima Krasner <dima@dimakrasner.com>
2026-01-08 19:19:54 +0200

    x

--- cluster/portability_test.go
+++ cluster/portability_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 Dima Krasner
+Copyright 2025, 2026 Dima Krasner
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
