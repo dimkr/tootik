7127c6e9ff7da20b0f99a89840105e155e680a69
Dima Krasner <dima@dimakrasner.com>
2026-01-28 21:09:15 +0200

    oops

--- migrations/061_backfill.go
+++ migrations/061_backfill.go
@@ -6,6 +6,6 @@ import (
 )
 
 func backfill(ctx context.Context, domain string, tx *sql.Tx) error {
-	_, err := tx.ExecContext(ctx, `CREATE INDEX historycreateupdateobjectid ON history(activity->>'$.object.id') WHERE (activity->>'$.type' = 'Create' or activity->>'$.type' = 'Update') and activity->>'$.object.id' IS NOT NULL`)
+	_, err := tx.ExecContext(ctx, `CREATE INDEX historycreateupdateobjectid ON history(activity->>'$.object.id') WHERE (activity->>'$.type' = 'Create' OR activity->>'$.type' = 'Update') AND activity->>'$.object.id' IS NOT NULL`)
 	return err
 }
