6792a6a057551aca562e1bdefbfde814a1874b0b
Dima Krasner <dima@dimakrasner.com>
2026-02-09 09:08:08 +0200

    cleanup

---
+++ cmd/demo/t.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type t struct {
+	ctx     context.Context
+	tempDir string
+}
+
+func (t) Parallel() {}
+
+func (t) Name() string {
+	return ""
+}
+
+func (t t) Context() context.Context {
+	return t.ctx
+}
+
+func (t t) TempDir() string {
+	return t.tempDir
+}
+
+func (t) Fatal(args ...any) {
+	panic(fmt.Sprint(args...))
+}
+
+func (t) Fatalf(format string, args ...any) {
+	panic(fmt.Sprintf(format, args...))
+}
