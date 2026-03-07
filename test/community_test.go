a0a300cc01e6eb6b216cc82ce6679ab5e147ed5a
Dima Krasner <dima@dimakrasner.com>
2026-03-07 18:06:19 +0200

    x

--- test/community_test.go
+++ test/community_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2024, 2025 Dima Krasner
+Copyright 2024 - 2026 Dima Krasner
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -60,7 +60,7 @@ func TestCommunity_NewThread(t *testing.T) {
 	assert.NoError(err)
 
 	assert.NoError(
-		server.inbox.Accept(
+		server.inbox.AcceptFollow(
 			context.Background(),
 			server.Alice,
 			httpsig.Key{},
