91021b595ade660f3937b093bbe0391e0b3d420c
Dima Krasner <dima@dimakrasner.com>
2026-05-18 18:58:47 +0300

    x

--- migrations/066_counters.go
+++ migrations/066_counters.go
@@ -14,10 +14,15 @@ func counters(ctx context.Context, domain string, tx *sql.Tx) error {
 		return err
 	}
 
+	if _, err := tx.ExecContext(ctx, `ALTER TABLE notes ADD COLUMN shares_count INTEGER DEFAULT 0`); err != nil {
+		return err
+	}
+
 	if _, err := tx.ExecContext(ctx, `
 		UPDATE notes SET
-		replies_count = (SELECT COUNT(*) FROM notes r WHERE r.object->>'$.inReplyTo' = notes.id),
-		quotes_count = (SELECT COUNT(*) FROM notes q WHERE q.object->>'$.quote' = notes.id)
+			replies_count = (SELECT COUNT(*) FROM notes r WHERE r.object->>'$.inReplyTo' = notes.id),
+			quotes_count = (SELECT COUNT(*) FROM notes quotes WHERE quotes.object->>'$.quote' = notes.id),
+			shares_count = (SELECT COUNT(*) FROM shares WHERE shares.note = notes.id)
 	`); err != nil {
 		return err
 	}
@@ -62,13 +67,34 @@ func counters(ctx context.Context, domain string, tx *sql.Tx) error {
 		return err
 	}
 
+	// Trigger for new shares
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TRIGGER shares_count_insert AFTER INSERT ON shares
+		BEGIN
+			UPDATE notes SET shares_count = shares_count + 1 WHERE id = NEW.note;
+		END
+	`); err != nil {
+		return err
+	}
+
+	// Trigger for deleted shares
+	if _, err := tx.ExecContext(ctx, `
+		CREATE TRIGGER shares_count_delete AFTER DELETE ON shares
+		BEGIN
+			UPDATE notes SET shares_count = MAX(0, shares_count - 1) WHERE id = OLD.note;
+		END
+	`); err != nil {
+		return err
+	}
+
 	// Trigger for new notes to handle replies/quotes that arrived before the parent
 	if _, err := tx.ExecContext(ctx, `
 		CREATE TRIGGER notes_initial_counts AFTER INSERT ON notes
 		BEGIN
 			UPDATE notes SET
 				replies_count = (SELECT COUNT(*) FROM notes WHERE object->>'$.inReplyTo' = NEW.id),
-				quotes_count = (SELECT COUNT(*) FROM notes WHERE object->>'$.quote' = NEW.id)
+				quotes_count = (SELECT COUNT(*) FROM notes WHERE object->>'$.quote' = NEW.id),
+				shares_count = (SELECT COUNT(*) FROM shares WHERE note = NEW.id)
 			WHERE id = NEW.id;
 		END
 	`); err != nil {
