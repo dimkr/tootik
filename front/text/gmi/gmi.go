82218b0b138852fb0a161d25d1bcaea9c055620e
Dima Krasner <dima@dimakrasner.com>
2026-06-16 22:00:12 +0300

    x

--- front/text/gmi/gmi.go
+++ front/text/gmi/gmi.go
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package gmi reads and writes Gemini responses.
+// Package gmi reads, writes and displays Gemini responses.
 package gmi
