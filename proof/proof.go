760ee501caea8fb2787b81e602b7c9ceb93895c3
Dima Krasner <dima@dimakrasner.com>
2026-08-15 16:17:34 +0300

    add support for validation of mldsa44-jcs-2024 proofs

--- proof/proof.go
+++ proof/proof.go
@@ -17,18 +17,22 @@ limitations under the License.
 // Package proof creates and verifies integrity proofs.
 //
 // See https://codeberg.org/fediverse/fep/src/branch/main/fep/8b32/fep-8b32.md for more details.
+//
+// In addition to eddsa-jcs-2022, this package supports mldsa44-jcs-2024; see https://www.w3.org/TR/vc-di-quantum-resistant-1.0/#cryptosuite-mldsa44-jcs-2024.
 package proof
 
 import (
 	"crypto"
 	"crypto/ed25519"
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"time"
 
 	"github.com/btcsuite/btcutil/base58"
+	"github.com/cloudflare/circl/sign/mldsa/mldsa44"
 	"github.com/dimkr/tootik/ap"
 	"github.com/dimkr/tootik/httpsig"
 	"github.com/gowebpki/jcs"
@@ -96,6 +100,9 @@ func create(key httpsig.Key, now time.Time, doc, context any) (ap.Proof, error) 
 	case ed25519.PrivateKey:
 		proof.CryptoSuite = "eddsa-jcs-2022"
 
+	case *mldsa44.PrivateKey:
+		proof.CryptoSuite = "mldsa44-jcs-2024"
+
 	default:
 		return ap.Proof{}, fmt.Errorf("wrong key type: %T", key.PrivateKey)
 	}
@@ -116,6 +123,14 @@ func create(key httpsig.Key, now time.Time, doc, context any) (ap.Proof, error) 
 	switch v := key.PrivateKey.(type) {
 	case ed25519.PrivateKey:
 		proof.Value = "z" + base58.Encode(ed25519.Sign(v, append(cfgHash[:], docHash[:]...)))
+
+	case *mldsa44.PrivateKey:
+		sig := make([]byte, mldsa44.SignatureSize)
+		if err := mldsa44.SignTo(v, append(cfgHash[:], docHash[:]...), nil, true, sig); err != nil {
+			return ap.Proof{}, err
+		}
+
+		proof.Value = "u" + base64.RawURLEncoding.EncodeToString(sig)
 	}
 
 	return proof, nil
@@ -179,6 +194,9 @@ func Verify(key crypto.PublicKey, proof ap.Proof, context any, raw []byte) error
 			options.Context = context
 		}
 
+	case "mldsa44-jcs-2024":
+		options.Context = context
+
 	default:
 		return fmt.Errorf("invalid cryptosuite: %s/%T", proof.CryptoSuite, key)
 	}
@@ -202,6 +220,25 @@ func Verify(key crypto.PublicKey, proof ap.Proof, context any, raw []byte) error
 		}
 
 		if !ed25519.Verify(edKey, append(cfgHash[:], docHash[:]...), base58.Decode(proof.Value[1:])) {
+			return errors.New("proof verification failed")
+		}
+
+	case "mldsa44-jcs-2024":
+		if proof.Value[0] != 'u' {
+			return errors.New("invalid value: " + proof.Value)
+		}
+
+		mlKey, ok := key.(*mldsa44.PublicKey)
+		if !ok {
+			return fmt.Errorf("wrong key type: %T", key)
+		}
+
+		sig, err := base64.RawURLEncoding.DecodeString(proof.Value[1:])
+		if err != nil {
+			return fmt.Errorf("failed to decode proof: %w", err)
+		}
+
+		if !mldsa44.Verify(mlKey, append(cfgHash[:], docHash[:]...), nil, sig) {
 			return errors.New("proof verification failed")
 		}
 	}
